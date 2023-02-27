413578e64081cbc75836582ecf0079ba743a63b2
Sheogorattio <no.passion.there.is.serenity@gmail.com>
2023-02-28 00:14:48 +0200

    Add project files.

---
+++ 28.02hw/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _28._02hw
+{
+    internal class Program
+    {
+        static void Swap<T>(ref T a, ref T b)
+        {
+            (a, b) = (b, a);
+        }
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 40;
+            Console.WriteLine("var a:{0}\nvar b{1}", a, b);
+            Swap(ref a, ref b);
+            Console.WriteLine("var a:{0}\nvar b{1}",a,b);
+            Console.ReadKey();
+        }
+    }
+}
